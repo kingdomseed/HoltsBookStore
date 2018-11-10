b0561e0d8e61d79611cb379060809e3cae1dadd1
Jason M Holt <kingdomseed@gmail.com>
2018-11-10 13:19:27 -0600

    Finally good grief.

--- app/src/main/java/com/holtnet/holtsbookstore/BookCursorAdapter.java
+++ app/src/main/java/com/holtnet/holtsbookstore/BookCursorAdapter.java
@@ -1,5 +1,6 @@
 package com.holtnet.holtsbookstore;
 
+import android.annotation.SuppressLint;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
@@ -12,13 +13,11 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.holtnet.holtsbookstore.data.BookContract.BookEntry;
 
 public class BookCursorAdapter extends CursorAdapter {
-
-    private int quantity = 0;
-    private TextView bookInStockItem;
 
     public BookCursorAdapter(Context context, Cursor c) {
         super(context, c, 0 /* flags */);
@@ -33,55 +32,56 @@ public class BookCursorAdapter extends CursorAdapter {
     @Override
     public void bindView(View view, final Context context, final Cursor cursor) {
 
+        final TextView bookInStockItem;
+
         Button sellButton = view.findViewById(R.id.sell_button);
         TextView bookNameItem = view.findViewById(R.id.bookNameItem);
         TextView bookPriceItem = view.findViewById(R.id.priceItem);
         bookInStockItem = view.findViewById(R.id.inStockItem);
 
+        int idColumnIndex = cursor.getColumnIndex(BookEntry._ID);
         int bookNameColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_NAME);
         int priceColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_PRICE);
         int quantityColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_QUANTITY);
 
+        final String id = cursor.getString(idColumnIndex);
         String bookName = cursor.getString(bookNameColumnIndex);
         double price = cursor.getDouble(priceColumnIndex);
-        quantity = cursor.getInt(quantityColumnIndex);
+        final int quantity = cursor.getInt(quantityColumnIndex);
 
         bookNameItem.setText(bookName);
         NumberFormat formatter = NumberFormat.getCurrencyInstance();
         bookPriceItem.setText(formatter.format(price));
 
-        updateQuantityTextView(quantity, bookInStockItem, context);
-
-        final int columnIndex = cursor.getColumnIndex(BookEntry._ID);
+        bookInStockItem.setText(new StringBuilder().append(context.getString(R.string.in_stock)).append(String.valueOf(quantity)).toString());
 
         sellButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Uri selectedBookUri = ContentUris.withAppendedId (BookEntry.CONTENT_URI, columnIndex);
-                ContentValues values = new ContentValues();
-                quantity--;
-                values.put ( BookEntry.COLUMN_QUANTITY, quantity);
-                int rowsAffected = context.getContentResolver().update(selectedBookUri, values, null, null);
-                if(rowsAffected == 0)
+                int changeQuantity = quantity;
+                if( changeQuantity > 0)
                 {
+                    changeQuantity--;
 
-                } else {
+                    ContentValues bookValues = new ContentValues();
+                    bookValues.put(BookEntry.COLUMN_QUANTITY, changeQuantity);
+                    bookInStockItem.setText(new StringBuilder().append(context.getString(R.string.in_stock)).append(String.valueOf(changeQuantity)).toString());
 
+                    Uri selectedBook = Uri.withAppendedPath(BookEntry.CONTENT_URI, id);
+                    int rowsAffected = context.getContentResolver().update(selectedBook, bookValues, null, null);
+
+                    if(rowsAffected > 0)
+                    {
+                        Toast.makeText(context, context.getString(R.string.sold_toast), Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(context, context.getString(R.string.error_selling_book), Toast.LENGTH_SHORT).show();
+                    }
                 }
-                updateQuantityTextView(quantity, bookInStockItem, context);
+                else {
+                    Toast.makeText(context, context.getString(R.string.not_in_stock_toast), Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
 
-    private void updateQuantityTextView(int quantity, TextView bookInStockItem, Context context)
-    {
-        if(quantity > 0)
-        {
-            StringBuilder stringBuilder = new StringBuilder(context.getString(R.string.in_stock));
-            bookInStockItem.setText(stringBuilder.append(quantity));
-        } else
-        {
-            bookInStockItem.setText(context.getString(R.string.out_of_stock));
-        }
-    }
 }
